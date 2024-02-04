75a8999574b5d8262f1cb16a9eacc1dd648b18ca
Ian Wang <ck1110892@gl.ck.tp.edu.tw>
2024-02-04 18:47:44 +0800

    ?

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/Constellation-ckcos14th.github.io" : "/Constellation-ckcos14th.github.io",
+  publicPath: "/Constellation-ckcos14th.github.io/"
 };
