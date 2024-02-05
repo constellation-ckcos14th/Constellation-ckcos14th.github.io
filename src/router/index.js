b1a0ac9b7f9d888c38ad17e8330b8b8d498a0f63
Ian Wang <ck1110892@gl.ck.tp.edu.tw>
2024-02-05 13:18:54 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import { comment } from "postcss";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
